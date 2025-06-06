387fa44fea34c4a0c9cb08c46d83b18003f6f1af
あ <k024g0101@g.neec.ac.jp>
2025-06-06 09:58:59 +0900

    DescriptorHeapの関数ファイル分け

---
+++ Source/DescriptorHeap.cpp
@@ -0,0 +1,24 @@
+#include"../Header/DescriptorHeap.h"
+
+#include<cassert> //assertも利用するため
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+//DescriptorHeapの作成関数
+Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> CreateDescriptorHeap(
+    const Microsoft::WRL::ComPtr<ID3D12Device>& device,
+    D3D12_DESCRIPTOR_HEAP_TYPE heapType,
+    UINT numDescriptors,
+    bool shaderVisible) {
+
+    //ディスクリプタヒープを生成する
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> descriptorHeap = nullptr;
+    D3D12_DESCRIPTOR_HEAP_DESC descriptorHeapDesc{};
+    descriptorHeapDesc.Type = heapType;//連打ーターゲットビュー用
+    descriptorHeapDesc.NumDescriptors = numDescriptors;
+    descriptorHeapDesc.Flags = shaderVisible ? D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE : D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+    HRESULT hr = device->CreateDescriptorHeap(&descriptorHeapDesc, IID_PPV_ARGS(&descriptorHeap));
+    //ディスクリプタヒープが作れなかったので起動できない
+    assert(SUCCEEDED(hr));
+    return descriptorHeap;
+}
--- main.cpp
+++ main.cpp
@@ -57,6 +57,7 @@ extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg
 #include"Header/CommandList.h"
 #include"Header/DXGIFactory.h"
 #include"Header/SwapChain.h"
+#include"Header/DescriptorHeap.h"
 
 #include"Header/Log.h"
 
@@ -233,24 +234,6 @@ Microsoft::WRL::ComPtr<ID3D12Resource> CreateBufferResource(
 
 };
 
-//DescriptorHeapの作成関数
-Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> CreateDescriptorHeap(
-    const Microsoft::WRL::ComPtr<ID3D12Device>& device,
-    D3D12_DESCRIPTOR_HEAP_TYPE heapType,
-    UINT numDescriptors,
-    bool shaderVisible) {
-
-    //ディスクリプタヒープを生成する
-    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> descriptorHeap = nullptr;
-    D3D12_DESCRIPTOR_HEAP_DESC descriptorHeapDesc{};
-    descriptorHeapDesc.Type = heapType;//連打ーターゲットビュー用
-    descriptorHeapDesc.NumDescriptors = numDescriptors;
-    descriptorHeapDesc.Flags = shaderVisible ? D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE : D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
-    HRESULT hr = device->CreateDescriptorHeap(&descriptorHeapDesc, IID_PPV_ARGS(&descriptorHeap));
-    //ディスクリプタヒープが作れなかったので起動できない
-    assert(SUCCEEDED(hr));
-    return descriptorHeap;
-}
 
 //インデックスのDescriptorHandleを取得する関数
 D3D12_CPU_DESCRIPTOR_HANDLE GetCPUDescriptorHandle(ID3D12DescriptorHeap* descriptorHeap, uint32_t descriptorSize, uint32_t index) {
