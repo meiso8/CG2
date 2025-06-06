ded2a1971b9a91ae53c69742280b632242c2e5a6
あ <k024g0101@g.neec.ac.jp>
2025-06-06 19:37:17 +0900

    Logのファイル修正

--- Header/Log.h
+++ Header/Log.h
@@ -3,41 +3,11 @@
 #include<string>//ログの文字列を出力するため
 
 //ログを出力する関数
-void Log(const std::string& message) {
-    OutputDebugStringA(message.c_str());
-}
+void Log(const std::string& message);
 
-void Log(std::ostream& os, const std::string& message) {
-    os << message << std::endl;
-    OutputDebugStringA(message.c_str());
-}
+void Log(std::ostream& os, const std::string& message);
 
 //string->wstringに変換する関数
-std::wstring ConvertString(const std::string& str) {
-    if (str.empty()) {
-        return std::wstring();
-    }
-
-    auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
-    if (sizeNeeded == 0) {
-        return std::wstring();
-    }
-    std::wstring result(sizeNeeded, 0);
-    MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
-    return result;
-}
-
+std::wstring ConvertString(const std::string& str);
 //wstring->stringに変換する関数
-std::string ConvertString(const std::wstring& str) {
-    if (str.empty()) {
-        return std::string();
-    }
-
-    auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
-    if (sizeNeeded == 0) {
-        return std::string();
-    }
-    std::string result(sizeNeeded, 0);
-    WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
-    return result;
-}
+std::string ConvertString(const std::wstring& str);
