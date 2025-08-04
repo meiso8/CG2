7c6e72d313b25fc600c766fa602a25c675a8e5ae
あ <k024g0101@g.neec.ac.jp>
2025-08-04 12:49:11 +0900

    移動完了

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include<numbers>
 #include"MyEngine.h"
-
-
+#include"Game/GameScene.h"
 
 #define WIN_WIDTH 1280
 #define WIN_HEIGHT 720
-
 
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -37,8 +35,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
      sound.SoundLoad(L"resources/Sounds/voice2.wav"),
      sound.SoundLoad(L"resources/Sounds/voice3.wav") };
 
-    bool isSound = false;
-    bool isVoiceSound = false;
 
 #pragma endregion
 
@@ -48,48 +44,45 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 #pragma region//Camera
 
     bool isDebug = false;
-
     Camera camera;
-    DebugCamera debugCamera;
-
-    Camera cameraSprite;
-    cameraSprite.Initialize(static_cast<float>(WIN_WIDTH), static_cast<float>(WIN_HEIGHT), true);
-
-#pragma endregion
-
     Transform cameraTransform{ { 1.0f, 1.0f, 1.0f }, { 0.0f,0.0f,0.0f }, { 0.0f,0.0f,-10.0f } };
     camera.SetTransform(cameraTransform);
     camera.Initialize(static_cast<float>(WIN_WIDTH), static_cast<float>(WIN_HEIGHT), false);
 
+    Camera cameraSprite;
+    cameraSprite.Initialize(static_cast<float>(WIN_WIDTH), static_cast<float>(WIN_HEIGHT), true);
+
+    DebugCamera debugCamera;
     debugCamera.Initialize(input, static_cast<float>(WIN_WIDTH), static_cast<float>(WIN_HEIGHT));
+#pragma endregion
 
-    Texture texture = Texture(myEngine.GetDevice(), myEngine.GetCommandList());
-    texture.Load("resources/dvd.png");
+    //Texture texture = Texture(myEngine.GetDevice(), myEngine.GetCommandList());
+    //texture.Load("resources/dvd.png");
 
-    //ShaderResourceViewを作る
-    ShaderResourceView srv = {};
-    srv.Create(texture, 1, myEngine.GetDevice(), myEngine.GetSrvDescriptorHeap());
+    ////ShaderResourceViewを作る
+    //ShaderResourceView srv = {};
+    //srv.Create(texture, 1, myEngine.GetDevice(), myEngine.GetSrvDescriptorHeap());
 
     Texture texture2 = Texture(myEngine.GetDevice(), myEngine.GetCommandList());
     texture2.Load("resources/white1x1.png");
 
     //ShaderResourceViewを作る
     ShaderResourceView srv2 = {};
-    srv2.Create(texture2, 2, myEngine.GetDevice(), myEngine.GetSrvDescriptorHeap());
+    srv2.Create(texture2, 1, myEngine.GetDevice(), myEngine.GetSrvDescriptorHeap());
 
     Texture texture3 = Texture(myEngine.GetDevice(), myEngine.GetCommandList());
     texture3.Load("resources/effect.png");
 
     //ShaderResourceViewを作る
     ShaderResourceView srv3 = {};
-    srv3.Create(texture3, 3, myEngine.GetDevice(), myEngine.GetSrvDescriptorHeap());
+    srv3.Create(texture3, 2, myEngine.GetDevice(), myEngine.GetSrvDescriptorHeap());
 
 
     DrawGrid grid = DrawGrid(myEngine.GetDevice(), camera, myEngine.GetModelConfig(0));
 
-    Sprite sprite;
-    sprite.Create(myEngine.GetDevice(), cameraSprite, myEngine.GetModelConfig(1));
-    sprite.SetSize(Vector2(256.0f, 128.0f));
+    //Sprite sprite;
+    //sprite.Create(myEngine.GetDevice(), cameraSprite, myEngine.GetModelConfig(1));
+    //sprite.SetSize(Vector2(256.0f, 128.0f));
 
 
     Texture texture4 = Texture(myEngine.GetDevice(), myEngine.GetCommandList());
@@ -97,7 +90,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
     //ShaderResourceViewを作る
     ShaderResourceView srv4 = {};
-    srv4.Create(texture4, 6, myEngine.GetDevice(), myEngine.GetSrvDescriptorHeap());
+    srv4.Create(texture4, 3, myEngine.GetDevice(), myEngine.GetSrvDescriptorHeap());
 
     Sprite playerSprite;
     playerSprite.Create(myEngine.GetDevice(), cameraSprite, myEngine.GetModelConfig(1));
@@ -106,50 +99,22 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     playerSprite.Update();
 
     Model bunnyModel(myEngine.GetModelConfig(0));
-    bunnyModel.Create("resources", "bunny.obj", myEngine.GetDevice(), myEngine.GetSrvDescriptorHeap(), 5);
-
-    Sphere sphere(myEngine.GetModelConfig(1));
-    sphere.Create(myEngine.GetDevice(), myEngine.GetSrvDescriptorHeap());
-
-
-    CollisionManager collisionManager;
-
-
-    Vector3 scale = {};
-    Vector3 rotation = {};
-    Vector3 translation = {};
-    Matrix4x4 worldMat = {};
-
-    rotation = { 0.0f,0.0f,0.0f };
-    scale = { 2.5f,2.5f,2.5f };
-    translation = { 0.0f,1.0f,0.0f };
-
-    MSG msg{};
-
-    float toCubeSpeed = 1.0f / 240.0f;
+    bunnyModel.Create("resources", "bunny.obj", myEngine.GetDevice(), myEngine.GetSrvDescriptorHeap(), 4);
+    assert(&bunnyModel);
 
     Vector4 worldColor = { 0.0f,0.0f,0.0f,1.0f };
 
     Vector2 speed = { 2.0f,2.0f };
+
     size_t currentIndex = 0;
     Vector4 colors[4] = { {1.0f,0.0f,0.0f,1.0f}, {0.0f,1.0f,0.0f,1.0f}, {0.0f,0.0f,1.0f,1.0f}, {1.0f,0.0f,1.0f,1.0f} };
     float timer = 0.0f;
 
+    GameScene gameScene;
+    gameScene.Init(myEngine,&bunnyModel);
 
-    Player player;
-    player.Init(bunnyModel);
 
-    std::list< Mirror*> mirrors;
-    for (int i = 0; i < 7; ++i) {
-        Mirror* mirror = new Mirror();
-
-        Model* mirrorModel = new Model(myEngine.GetModelConfig(0));
-        mirrorModel->Create("resources/mirror", "mirror.obj", myEngine.GetDevice(), myEngine.GetSrvDescriptorHeap(), 4);
-
-        mirror->Init(*mirrorModel);
-        mirror->SetTranslate({ (i + 1) * 4.0f,0.0f,0.0f });
-        mirrors.push_back(mirror);
-    }
+    MSG msg{};
 
     // =============================================
     //ウィンドウのxボタンが押されるまでループ メインループ
@@ -176,6 +141,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
             timer++;
             float t = timer / 240.0f;
 
+            gameScene.Update(sound,soundData1,soundData2,voiceData);
+
 
 #ifdef _DEBUG
 
@@ -201,10 +168,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
             }
 
-            ImGui::Begin("WorldMatrix");
-            const char* items[] = { "SPHERE" };
-            debugUI.WorldMatrixUpdate(scale, rotation, translation, items[0]);
-            ImGui::End();
+            //ImGui::Begin("WorldMatrix");
+            //const char* items[] = { "SPHERE" };
+            //debugUI.WorldMatrixUpdate(scale, rotation, translation, items[0]);
+            //ImGui::End();
 
             {
                 Vector3 direction = myEngine.GetDirectionalLightData().direction;
@@ -219,37 +186,22 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
                 static int light_current = 2;
 
                 ImGui::Combo("LightMode", &light_current, lights, IM_ARRAYSIZE(lights));
-                sphere.GetMaterial()->lightType = light_current % 3;
+         /*       sphere.GetMaterial()->lightType = light_current % 3;*/
                 bunnyModel.GetMaterial()->lightType = light_current % 3;
 
                 ImGui::End();
             }
 
-            debugUI.SphereUpdate(sphere);
+            //debugUI.SphereUpdate(sphere);
             debugUI.SpriteUpdate(playerSprite);
-
             debugUI.InputUpdate(*input);
             debugUI.Color(worldColor);
-
-            debugUI.DebugMirror(mirrors);
+            debugUI.DebugMirror(gameScene.GetMirrors());
 
 #endif
 
-            worldMat = MakeAffineMatrix(scale, rotation, translation);
 
-            sphere.SetUVScale({ 1.0f,50.0f,1.0f });
-            sphere.GetUVTransform().translate += { 0.0f, 1.0f / 60.0f, 0.0f };
-            sphere.UpdateUV();
-            sphere.SetColor(Lerp(colors[currentIndex], colors[(currentIndex + 1) % 4], t));
-
-            bunnyModel.GetWaveData(0).amplitude = 1.0f / 16.0f;
-            bunnyModel.GetWaveData(0).time += 1.0f / 60.0f;
-
-            sphere.GetExpansionData().cube += toCubeSpeed;
-            if (sphere.GetExpansionData().cube <= 0.0f || sphere.GetExpansionData().cube >= 1.0f) {
-                toCubeSpeed *= -1.0f;
-            }
-            sprite.SetColor(Lerp(colors[currentIndex], colors[(currentIndex + 1) % 4], t));
+     /*       sprite.SetColor(Lerp(colors[currentIndex], colors[(currentIndex + 1) % 4], t));
             sprite.GetTranslateRef() += {speed.x, speed.y, 0.0f};
 
             if (sprite.GetTranslateRef().x > myEngine.GetWC().GetClientWidth() - sprite.GetSize().x || sprite.GetTranslateRef().x < 0.0f) {
@@ -260,12 +212,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
                 speed.y *= -1.0f;
             }
 
-            sprite.Update();
+            sprite.Update();*/
 
-            if (input->IsTriggerKey(DIK_SPACE)) {
-                //音声再生
-                sound.SoundPlay(soundData2, 0.5f);
-            }
+    
 
             if (input->IsTriggerKey(DIK_RETURN)) {
                 debugCamera.SetIsOrthographic(true);
@@ -284,62 +233,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
                 debugCamera.Update();
 
             } else {
-                Vector3 currentCamPos = camera.GetTranslate(); // カメラの現在位置
-                Vector3 targetCamPos = player.GetWorldPosition();
-                targetCamPos.z -= 20.0f; // プレイヤーの後ろに配置
-
-                // 補間：現在位置 → 目標位置へ滑らかに移動（αは追従の速さ）
-                float followSpeed = 0.1f; // 0.0〜1.0（小さいほどゆっくり）
-                Vector3 smoothedPos = Lerp(currentCamPos, targetCamPos, followSpeed);
-
-                camera.SetTarnslate(smoothedPos);
-                camera.Update();
+                gameScene.CameraUpdate(camera);
             }
-
-
-            player.Update();
-            if (player.IsHit()) {
-
-                if (!isVoiceSound) {
-                    isVoiceSound = true;
-
-                }
-
-
-            }
-
-            for (Mirror* mirror : mirrors) {
-                mirror->Update();
-                if (mirror->IsDead()) {
-
-                    isSound = true;
-
-                }
-            }
-
-            mirrors.remove_if([](Mirror* mirror) {
-                if (mirror->IsDead()) {
-
-                    delete mirror;
-                    return true;
-                }
-                return false;
-                });
-
-
-            if (isSound) {
-                sound.SoundPlay(soundData1, 0.125f);
-                isSound = false;
-            }
-
-            if (isVoiceSound) {
-                sound.SoundPlay(voiceData[rand() % 3], 0.125f);
-                isVoiceSound = false;
-            }
-
-            collisionManager.RegisterList(&player, mirrors);
-            collisionManager.CheckAllCollisions();
-            collisionManager.ClearList();
 
 #pragma region //描画
 
@@ -349,19 +244,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
             //グリッドの描画
             grid.Draw(srv2);
 
-            for (Mirror* mirror : mirrors) {
-                mirror->Draw(camera);
-            }
-
-            player.Draw(camera);
-
-            sphere.PreDraw();
-            sphere.Draw(worldMat, camera, srv3);
-
-            sprite.PreDraw();
-            sprite.Draw(srv);
-
-            playerSprite.Draw(srv4);
+            gameScene.Draw(camera,srv3,srv4,playerSprite);
 
             myEngine.PostCommandSet();
 #pragma endregion
@@ -369,13 +252,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         }
     }
 
-    for (Mirror* mirror : mirrors) {
 
-        delete mirror;
-
-    }
-
-    mirrors.clear();
 
     myEngine.End();
 
