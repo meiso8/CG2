7eb7d45ef95d24cb87b4cfc4ad72ae8557630c93
あ <k024g0101@g.neec.ac.jp>
2025-09-30 21:06:45 +0900

    test

--- project/Source/main.cpp
+++ project/Source/main.cpp
@@ -8,7 +8,7 @@
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
     MyEngine myEngine;
-    myEngine.Create(L"MyEngine_GE3_", WIN_WIDTH, WIN_HEIGHT);
+    myEngine.Create(L"MyEngine_GE3_Develop", WIN_WIDTH, WIN_HEIGHT);
     //DirectX初期化処理の末尾に追加する
 //音声クラスの作成
     Sound sound;
